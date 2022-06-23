702eae41ba8ba98072ddfca5413960f6455d8dea
tinadam <94481038+tinadam@users.noreply.github.com>
2022-06-23 13:27:11 -0400

    june 23_v2

--- js/index.js
+++ js/index.js
@@ -35,3 +35,25 @@ $(function() {
 
     });
 });
+
+
+
+$(document).ready(function() {
+    //change the integers below to match the height of your upper div, which I called
+    //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+    //to figure out what the scroll position is when exactly you want to fix the nav
+    //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
+    //you know the position.
+    $(window).scroll(function () { 
+  
+      console.log($(window).scrollTop());
+  
+      if ($(window).scrollTop() > 550) {
+        $('.case_nav').addClass('case_nav-fixed');
+      }
+  
+      if ($(window).scrollTop() < 551) {
+        $('.case_nav').removeClass('case_nav-fixed');
+      }
+    });
+  });
