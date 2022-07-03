6353094dfc4b96dcfaf9d877dee36dc5ece70b7d
tinadam <94481038+tinadam@users.noreply.github.com>
2022-07-02 23:01:50 -0400

    july 2_v2

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ $(function() {
 
         if( window_bottom > target_bottom ){
 
-            $(this).animate({'opacity':'1'},1000);
+            $(this).animate({'opacity':'1'},1500);
 
             }
         }); 
@@ -28,13 +28,25 @@ $(function() {
 
         if( window_bottom > target_bottom ){
 
-            $(this).animate({'opacity':'1'},1000);
+            $(this).animate({'opacity':'1'},1500);
 
             }
         }); 
 
     });
 });
+
+$(document).ready(function() {
+        $(window).scroll( function(){
+            $('.fade_in').each( function(i){
+                var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                if( bottom_of_window > bottom_of_element ){
+                    $(this).animate({'opacity':'1'},1500);
+                }
+            });
+        });
+    });
 
 $(document).ready(function() {
     var navpos = $('#casenav').offset();
