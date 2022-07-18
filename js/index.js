3b165a8815a76e907c0c8a057e879aa2bae87ece
tinadam <94481038+tinadam@users.noreply.github.com>
2022-07-18 18:58:55 -0400

    july 18_v9

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
                 var bottom_of_element = $(this).offset().top + $(this).outerHeight();
                 var bottom_of_window = $(window).scrollTop() + $(window).height();
                 if( bottom_of_window > bottom_of_element ){
-                    $(this).animate({'opacity':'1'},1500);
+                    $(this).animate({'opacity':'1'},500);
                 }
             });
         });
